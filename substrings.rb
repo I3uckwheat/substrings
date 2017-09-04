c915bdc49bd75482e73ee540dd78179fa8a6cd04
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-04 11:29:35 -0500

    finds matches

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,17 @@
 def substrings(word, dictionary)
-
+  matches = {}
+  word.split().each do |word|
+    dictionary.each do |i|
+      if word.match(i) 
+        
+      end
+    end
+  end
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings("below", dictionary)
+
+
+# HINT: USE REGEX
