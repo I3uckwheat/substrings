04c4bb61b0157639c53e61f7e2680354b365d7b9
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-04 16:32:37 -0500

    write to hash

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,17 @@
 def substrings(word, dictionary)
   matches = {}
   word.split().each do |word|
+    count = 0
     dictionary.each do |i|
       if word.match(i) 
-        
+        count += 1
+        matches[i] = count
       end
     end
   end
+  puts matches
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
-
-
-# HINT: USE REGEX
+substrings("Howdy partner, sit down! How's it going?", dictionary)
