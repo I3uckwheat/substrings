1cf42bceb483b3fc9aaeb8fe4c6a295f56508227
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-03 23:12:46 -0500

    groundwork

---
+++ substrings.rb
@@ -0,0 +1,7 @@
+def substrings(word, dictionary)
+
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
